a511bccc8d3c710f82ae5b2200006a6025a2bbef
Jogis <ronipihajoki@proton.me>
2025-11-07 19:03:07 +0200

    Update App.java

--- jaakaappi/src/App.java
+++ jaakaappi/src/App.java
@@ -4,40 +4,46 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner (System.in);
         ArrayList<Integer>lampotila = new ArrayList<Integer>();
-    // jaakaappi olio1 = new jaakaappi();
+    jaakaappi olio1 = new jaakaappi(true,"Epoc",lampotila.get(0));
     
+    boolean ovi = false;
+
     System.out.println("Aseta jaakaapin lampotila: ");
     int ltarvo = Integer.parseInt(in.nextLine());
     lampotila.add(0,ltarvo);
 
-    System.out.println(lampotila.get(0));
-    }
-} // Main loppuu
-// class jaakaappi{
-//     boolean onkoPaalla;
-//     String merkki;
-//     int lampoTila;
-//     boolean oviAuki;
+    System.out.println("Onko ovi auki?");
+    String onko = in.nextLine();
+    if(onko.equalsIgnoreCase("on")){
+        ovi = true;
+    }else{ovi = false;}
 
-//     public jaakaappi(){ // Parametriton muodostin
-//         onkoPaalla = false;
-//         merkki = "";
-//         lampoTila = 0;
-//         oviAuki = false; 
-//     }
-//     public vaihdaLampotila(String vaihto){
-//         vaihto = 
-//     }
-//     public tulostaLampotila(){
-//         System.out.println("Lampotila on :" + lampoTila);
-//     }
-//     public void virtaPaalla(){
-//         onkoPaalla = true;
-//     }
-//     public void oviAuki(){
-//         if(oviAuki){
-//             System.out.println("Valo palaa.");
-//         } else {System.out.println("Valo ei pala.");}
-//     }
-//     public void 
-// } // Class jaakaappi loppuu
+    olio1.virtaPaalla();
+    olio1.tulostaLampotila();
+    olio1.oviAuki();
+    }
+}  // Main loppuu
+class jaakaappi{
+    boolean onkoPaalla;
+    String merkki;
+    int lampoTila;
+    boolean oviAuki;
+
+    public jaakaappi(boolean op, String m, Integer lT){ // taysiparametri muodostin
+        onkoPaalla = op;
+        merkki = m;
+        lampoTila = lT;
+        oviAuki = false;
+    }
+    public void tulostaLampotila(){
+        System.out.println("Lampotila on :" + lampoTila);
+    }
+    public void virtaPaalla(){
+        onkoPaalla = true;
+    }
+    public void oviAuki(){
+        if(oviAuki){
+            System.out.println("Valo palaa.");
+        } else {System.out.println("Valo ei pala.");}
+    }
+} // Class jaakaappi loppuu
