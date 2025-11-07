b977351463780f109fba67b4b5bdfac1bf0f7653
Jogis <ronipihajoki@proton.me>
2025-11-07 18:50:42 +0200

    Update App.java

--- jaakaappi/src/App.java
+++ jaakaappi/src/App.java
@@ -1,5 +1,43 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner (System.in);
+        ArrayList<Integer>lampotila = new ArrayList<Integer>();
+    // jaakaappi olio1 = new jaakaappi();
+    
+    System.out.println("Aseta jaakaapin lampotila: ");
+    int ltarvo = Integer.parseInt(in.nextLine());
+    lampotila.add(0,ltarvo);
+
+    System.out.println(lampotila.get(0));
     }
-}
+} // Main loppuu
+// class jaakaappi{
+//     boolean onkoPaalla;
+//     String merkki;
+//     int lampoTila;
+//     boolean oviAuki;
+
+//     public jaakaappi(){ // Parametriton muodostin
+//         onkoPaalla = false;
+//         merkki = "";
+//         lampoTila = 0;
+//         oviAuki = false; 
+//     }
+//     public vaihdaLampotila(String vaihto){
+//         vaihto = 
+//     }
+//     public tulostaLampotila(){
+//         System.out.println("Lampotila on :" + lampoTila);
+//     }
+//     public void virtaPaalla(){
+//         onkoPaalla = true;
+//     }
+//     public void oviAuki(){
+//         if(oviAuki){
+//             System.out.println("Valo palaa.");
+//         } else {System.out.println("Valo ei pala.");}
+//     }
+//     public void 
+// } // Class jaakaappi loppuu
