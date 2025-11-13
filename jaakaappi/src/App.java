b2f3424e5fe4f592eb2623e3fbfdbdcba4e6838e
Jogis <ronipihajoki@proton.me>
2025-11-13 17:13:53 +0200

    Update App.java

--- jaakaappi/src/App.java
+++ jaakaappi/src/App.java
@@ -2,16 +2,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
+        ArrayList<Integer>asteet = new ArrayList<Integer>();
         Scanner in = new Scanner (System.in);
         jaakaappi olio1 = new jaakaappi();
 
     boolean auki = false;
 
-    System.out.println("Onko ovi auki?");
+    System.out.println("Onko ovi auki? Vastaa: On/Ei");
         String ovi = in.nextLine();
 
     System.out.println("Monta astetta jaakaappi on?");
-        int lampo = Integer.parseInt(in.nextLine());
+        asteet.add(Integer.parseInt(in.nextLine()));
 
     if(ovi.equalsIgnoreCase("on")){
         auki = true;
@@ -19,8 +20,17 @@ public class App {
     
     // Kutsutaan olioita
     olio1.virtaPaalla();
-    olio1.tulostaLampotila(lampo);
+    olio1.tulostaLampotila(asteet.get(0));
     olio1.valoPaalla(auki);
+
+    System.out.println("Onko jaakaapissa liian kylma? Halutako vaihtaa asteita? Vastaa: Kylla/Ei");
+        String kysely = in.nextLine();
+            if(kysely.equalsIgnoreCase("kylla")){
+                System.out.println("Anna uusi lampotila: ");
+                    int uusiaste = Integer.parseInt(in.nextLine());
+                asteet.set(0, uusiaste);
+    olio1.tulostaLampotila(asteet.get(0));
+            }
     }
 }  // Main loppuu
 
@@ -33,7 +43,7 @@ class jaakaappi{
         onkoPaalla = true;
         merkki = "Epoc";
     }
-    public void tulostaLampotila(int lampoTila){
+    public void tulostaLampotila(Integer lampoTila){ // Miksi tama ei ole INT?!?!?
         System.out.println("Lampotila on " + lampoTila);
     }
     public void virtaPaalla(){
