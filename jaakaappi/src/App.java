1c215cdc5d90c98c01d952d62d945345b93c02f4
Jogis <ronipihajoki@proton.me>
2025-11-13 17:23:31 +0200

    Update App.java

--- jaakaappi/src/App.java
+++ jaakaappi/src/App.java
@@ -6,23 +6,23 @@ public class App {
         Scanner in = new Scanner (System.in);
         jaakaappi olio1 = new jaakaappi();
 
-    boolean auki = false;
-
+    boolean auki = false; // Asetetaan oven default arvo false. Koska yleisesti parempi jos jaakaappi on kiinni.
     System.out.println("Onko ovi auki? Vastaa: On/Ei");
         String ovi = in.nextLine();
+            if(ovi.equalsIgnoreCase("on")){
+                auki = true;
+            } // Kysyy kayttajalta onko jaakaappi auki vai kiinni. (Palaako valo vai ei)
 
     System.out.println("Monta astetta jaakaappi on?");
-        asteet.add(Integer.parseInt(in.nextLine()));
+        asteet.add(Integer.parseInt(in.nextLine())); // Kayttaja asettaa jaakaapin lampotilan.
 
-    if(ovi.equalsIgnoreCase("on")){
-        auki = true;
-    } else {auki = false;}
-    
+
     // Kutsutaan olioita
     olio1.virtaPaalla();
     olio1.tulostaLampotila(asteet.get(0));
     olio1.valoPaalla(auki);
 
+// Niin sanottu "Tiedon muunnos olio."
     System.out.println("Onko jaakaapissa liian kylma? Halutako vaihtaa asteita? Vastaa: Kylla/Ei");
         String kysely = in.nextLine();
             if(kysely.equalsIgnoreCase("kylla")){
@@ -30,29 +30,32 @@ public class App {
                     int uusiaste = Integer.parseInt(in.nextLine());
                 asteet.set(0, uusiaste);
     olio1.tulostaLampotila(asteet.get(0));
-            }
+            } // Jos jaakaapin lampotila on sopiva jo alusta, niin vastaus ei ohittaa taman kyselyn.
     }
 }  // Main loppuu
 
 class jaakaappi{
     boolean onkoPaalla;
     String merkki;
+    // int lampoTila = 0;
     // boolean oviAuki;
 
-    public jaakaappi(){ // taysiparametri muodostin
+    public jaakaappi(){ // Ei kysyttavaa
         onkoPaalla = true;
         merkki = "Epoc";
     }
     public void tulostaLampotila(Integer lampoTila){ // Miksi tama ei ole INT?!?!?
         System.out.println("Lampotila on " + lampoTila);
     }
-    public void virtaPaalla(){
+    public void virtaPaalla(){ // Hiukan turha methodi kokonaisuudessaan.
         onkoPaalla = true;
         System.out.println("Jaakaapissa on virta");
     }
-    public void valoPaalla(boolean oviAuki){
+    public void valoPaalla(boolean oviAuki){ // Onko booleanissa joku fiksumpi tapa kuin String if-lause user inputtiin.
         if(oviAuki){
             System.out.println("Valo palaa.");
         } else {System.out.println("Valo ei pala.");}
     }
 } // Class jaakaappi loppuu
+
+// Kiitos :) -Roni
