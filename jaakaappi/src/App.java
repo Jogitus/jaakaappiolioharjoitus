8c45b8c331158de6b7dbd7286e31e243ff5af27b
Jogis <ronipihajoki@proton.me>
2025-11-13 17:03:29 +0200

    Update App.java

--- jaakaappi/src/App.java
+++ jaakaappi/src/App.java
@@ -3,33 +3,44 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner (System.in);
-    jaakaappi olio1 = new jaakaappi(true,"Epoc",6, true);
+        jaakaappi olio1 = new jaakaappi();
+
+    boolean auki = false;
+
+    System.out.println("Onko ovi auki?");
+        String ovi = in.nextLine();
+
+    System.out.println("Monta astetta jaakaappi on?");
+        int lampo = Integer.parseInt(in.nextLine());
+
+    if(ovi.equalsIgnoreCase("on")){
+        auki = true;
+    } else {auki = false;}
     
-    
+    // Kutsutaan olioita
     olio1.virtaPaalla();
-    olio1.tulostaLampotila();
-    olio1.oviAuki();
+    olio1.tulostaLampotila(lampo);
+    olio1.valoPaalla(auki);
     }
 }  // Main loppuu
+
 class jaakaappi{
     boolean onkoPaalla;
     String merkki;
-    int lampoTila;
-    boolean oviAuki;
+    // boolean oviAuki;
 
-    public jaakaappi(boolean op, String m, Integer lT, boolean oA){ // taysiparametri muodostin
-        onkoPaalla = op;
-        merkki = m;
-        lampoTila = lT;
-        oviAuki = oA;
+    public jaakaappi(){ // taysiparametri muodostin
+        onkoPaalla = true;
+        merkki = "Epoc";
     }
-    public void tulostaLampotila(){
+    public void tulostaLampotila(int lampoTila){
         System.out.println("Lampotila on " + lampoTila);
     }
     public void virtaPaalla(){
         onkoPaalla = true;
+        System.out.println("Jaakaapissa on virta");
     }
-    public void oviAuki(){
+    public void valoPaalla(boolean oviAuki){
         if(oviAuki){
             System.out.println("Valo palaa.");
         } else {System.out.println("Valo ei pala.");}
